8a3ab4f7cb58dadb56467acc3fa422b2004f5612
Maciej Winnicki <mthenw@users.noreply.github.com>
2020-05-18 22:46:25 +0200

    chore: prettier

--- index.js
+++ index.js
@@ -1,109 +1,112 @@
-const util = require('util')
-const exec = util.promisify(require('child_process').exec)
-const merge = require('lodash.merge')
-const pMap = require('p-map')
-const os = require('os')
-const prettyHrtime = require('pretty-hrtime')
-const chalk = require('chalk')
-const path = require('path')
+const util = require("util");
+const exec = util.promisify(require("child_process").exec);
+const merge = require("lodash.merge");
+const pMap = require("p-map");
+const os = require("os");
+const prettyHrtime = require("pretty-hrtime");
+const chalk = require("chalk");
+const path = require("path");
 
 const ConfigDefaults = {
-  baseDir: '.',
-  binDir: '.bin',
+  baseDir: ".",
+  binDir: ".bin",
   cgo: 0,
-  cmd: 'GOOS=linux go build -ldflags="-s -w"'
-}
+  cmd: 'GOOS=linux go build -ldflags="-s -w"',
+};
 
-const GoRuntime = 'go1.x'
+const GoRuntime = "go1.x";
 
 module.exports = class Plugin {
   constructor(serverless, options) {
-    this.serverless = serverless
-    this.options = options || {}
-    this.isInvoking = false
+    this.serverless = serverless;
+    this.options = options || {};
+    this.isInvoking = false;
 
     this.hooks = {
-      'before:deploy:function:packageFunction': this.compileFunction.bind(this),
-      'before:package:createDeploymentArtifacts': this.compileFunctions.bind(
+      "before:deploy:function:packageFunction": this.compileFunction.bind(this),
+      "before:package:createDeploymentArtifacts": this.compileFunctions.bind(
         this
       ),
       // Because of https://github.com/serverless/serverless/blob/master/lib/plugins/aws/invokeLocal/index.js#L361
       // plugin needs to compile a function and then ignore packaging.
-      'before:invoke:local:invoke': this.compileFunctionAndIgnorePackage.bind(
+      "before:invoke:local:invoke": this.compileFunctionAndIgnorePackage.bind(
         this
       ),
-      'go:build:build': this.compileFunctions.bind(this)
-    }
+      "go:build:build": this.compileFunctions.bind(this),
+    };
 
     this.commands = {
       go: {
-        usage: 'Manage Go functions',
-        lifecycleEvents: ['go'],
+        usage: "Manage Go functions",
+        lifecycleEvents: ["go"],
         commands: {
-          build: { usage: 'Build all Go functions', lifecycleEvents: ['build'] }
-        }
-      }
-    }
+          build: {
+            usage: "Build all Go functions",
+            lifecycleEvents: ["build"],
+          },
+        },
+      },
+    };
   }
 
   async compileFunction() {
-    const name = this.options.function
-    const func = this.serverless.service.functions[this.options.function]
+    const name = this.options.function;
+    const func = this.serverless.service.functions[this.options.function];
 
-    const timeStart = process.hrtime()
-    await this.compile(name, func)
-    const timeEnd = process.hrtime(timeStart)
+    const timeStart = process.hrtime();
+    await this.compile(name, func);
+    const timeEnd = process.hrtime(timeStart);
 
     this.serverless.cli.consoleLog(
       `Go Plugin: ${chalk.yellow(
         `Compilation time (${name}): ${prettyHrtime(timeEnd)}`
       )}`
-    )
+    );
   }
 
   async compileFunctions() {
     if (this.isInvoking) {
-      return
+      return;
     }
 
-    let names = Object.keys(this.serverless.service.functions)
+    let names = Object.keys(this.serverless.service.functions);
 
-    const timeStart = process.hrtime()
+    const timeStart = process.hrtime();
     await pMap(
       names,
-      async name => {
-        const func = this.serverless.service.functions[name]
-        await this.compile(name, func)
+      async (name) => {
+        const func = this.serverless.service.functions[name];
+        await this.compile(name, func);
       },
       { concurrency: os.cpus().length }
-    )
-    const timeEnd = process.hrtime(timeStart)
+    );
+    const timeEnd = process.hrtime(timeStart);
 
     this.serverless.cli.consoleLog(
-      `Go Plugin: ${chalk.yellow('Compilation time: ' + prettyHrtime(timeEnd))}`
-    )
+      `Go Plugin: ${chalk.yellow("Compilation time: " + prettyHrtime(timeEnd))}`
+    );
   }
 
   compileFunctionAndIgnorePackage() {
-    this.isInvoking = true
-    return this.compileFunction()
+    this.isInvoking = true;
+    return this.compileFunction();
   }
 
   async compile(name, func) {
-    const config = this.getConfig()
+    const config = this.getConfig();
 
-    const runtime = func.runtime || this.serverless.service.provider.runtime
+    const runtime = func.runtime || this.serverless.service.provider.runtime;
     if (runtime !== GoRuntime) {
-      return
+      return;
     }
 
-    const absHandler = path.resolve(config.baseDir)
-    const absBin = path.resolve(config.binDir)
-    const compileBinPath = path.join(path.relative(absHandler, absBin), name) // binPath is based on cwd no baseDir
+    const absHandler = path.resolve(config.baseDir);
+    const absBin = path.resolve(config.binDir);
+    const compileBinPath = path.join(path.relative(absHandler, absBin), name); // binPath is based on cwd no baseDir
     try {
       const [env, command] = parseCommand(
         `${config.cmd} -o ${compileBinPath} ${func.handler}`
-      )
+      );
       await exec(command, {
         cwd: config.baseDir,
         env: Object.assign(
@@ -111,67 +114,67 @@ module.exports = class Plugin {
           process.env,
           { CGO_ENABLED: config.cgo.toString() },
           env
-        )
-      })
+        ),
+      });
     } catch (e) {
       this.serverless.cli.consoleLog(
         `Go Plugin: ${chalk.yellow(
           `Error compiling "${name}" function (cwd: ${config.baseDir}): ${e.message}`
         )}`
-      )
-      process.exit(1)
+      );
+      process.exit(1);
     }
 
-    let binPath = path.join(config.binDir, name)
-    if (process.platform === 'win32') {
-      binPath = binPath.replace(/\\/g, '/');
+    let binPath = path.join(config.binDir, name);
+    if (process.platform === "win32") {
+      binPath = binPath.replace(/\\/g, "/");
     }
-    this.serverless.service.functions[name].handler = binPath
+    this.serverless.service.functions[name].handler = binPath;
     const packageConfig = {
       individually: true,
       exclude: [`./**`],
-      include: [binPath]
-    }
+      include: [binPath],
+    };
     if (this.serverless.service.functions[name].package) {
       packageConfig.include = packageConfig.include.concat(
         this.serverless.service.functions[name].package.include
-      )
+      );
     }
-    this.serverless.service.functions[name].package = packageConfig
+    this.serverless.service.functions[name].package = packageConfig;
   }
 
   getConfig() {
-    let config = ConfigDefaults
+    let config = ConfigDefaults;
     if (this.serverless.service.custom && this.serverless.service.custom.go) {
-      config = merge(config, this.serverless.service.custom.go)
+      config = merge(config, this.serverless.service.custom.go);
     }
-    return config
+    return config;
   }
-}
+};
 
-const envSetterRegex = /^(\w+)=('(.*)'|"(.*)"|(.*))/
+const envSetterRegex = /^(\w+)=('(.*)'|"(.*)"|(.*))/;
 function parseCommand(cmd) {
-  const args = cmd.split(' ')
-  const envSetters = {}
-  let command = ''
+  const args = cmd.split(" ");
+  const envSetters = {};
+  let command = "";
   for (let i = 0; i < args.length; i++) {
-    const match = envSetterRegex.exec(args[i])
+    const match = envSetterRegex.exec(args[i]);
     if (match) {
-      let value
-      if (typeof match[3] !== 'undefined') {
-        value = match[3]
-      } else if (typeof match[4] === 'undefined') {
-        value = match[5]
+      let value;
+      if (typeof match[3] !== "undefined") {
+        value = match[3];
+      } else if (typeof match[4] === "undefined") {
+        value = match[5];
       } else {
-        value = match[4]
+        value = match[4];
       }
 
-      envSetters[match[1]] = value
+      envSetters[match[1]] = value;
     } else {
-      command = args.slice(i).join(' ')
-      break
+      command = args.slice(i).join(" ");
+      break;
     }
   }
 
-  return [envSetters, command]
+  return [envSetters, command];
 }
--- index.test.js
+++ index.test.js
@@ -1,327 +1,327 @@
-const proxyquire = require('proxyquire')
-const merge = require('lodash.merge')
-const sinon = require('sinon')
-const chai = require('chai')
-const expect = chai.expect
+const proxyquire = require("proxyquire");
+const merge = require("lodash.merge");
+const sinon = require("sinon");
+const chai = require("chai");
+const expect = chai.expect;
 
-chai.use(require('sinon-chai'))
+chai.use(require("sinon-chai"));
 
-describe('Go Plugin', () => {
-  let sandbox
-  let execStub
-  let Plugin
+describe("Go Plugin", () => {
+  let sandbox;
+  let execStub;
+  let Plugin;
 
   beforeEach(() => {
-    sandbox = sinon.createSandbox()
-    execStub = sandbox.stub().resolves({ stdin: null, stdout: null })
-    Plugin = proxyquire('./index.js', {
+    sandbox = sinon.createSandbox();
+    execStub = sandbox.stub().resolves({ stdin: null, stdout: null });
+    Plugin = proxyquire("./index.js", {
       util: {
-        promisify: () => execStub
-      }
-    })
-  })
+        promisify: () => execStub,
+      },
+    });
+  });
 
   afterEach(() => {
-    sandbox.restore()
-  })
+    sandbox.restore();
+  });
 
-  describe('`serverless deploy`', () => {
-    it('compiles only Go functions', async () => {
+  describe("`serverless deploy`", () => {
+    it("compiles only Go functions", async () => {
       // given
       const config = merge(
         {
           service: {
             functions: {
               testFunc1: {
-                name: 'testFunc1',
-                runtime: 'nodejs10.x',
-                handler: 'functions/func1'
+                name: "testFunc1",
+                runtime: "nodejs10.x",
+                handler: "functions/func1",
               },
               testFunc2: {
-                name: 'testFunc2',
-                runtime: 'go1.x',
-                handler: 'functions/func2/main.go'
+                name: "testFunc2",
+                runtime: "go1.x",
+                handler: "functions/func2/main.go",
               },
               testFunc3: {
-                name: 'testFunc3',
-                runtime: 'go1.x',
-                handler: 'functions/func3'
-              }
-            }
-          }
+                name: "testFunc3",
+                runtime: "go1.x",
+                handler: "functions/func3",
+              },
+            },
+          },
         },
         serverlessStub
-      )
-      const plugin = new Plugin(config)
+      );
+      const plugin = new Plugin(config);
 
       // when
-      await plugin.hooks['before:package:createDeploymentArtifacts']()
+      await plugin.hooks["before:package:createDeploymentArtifacts"]();
 
       // then
       expect(config.service.functions.testFunc2.handler).to.equal(
         `.bin/testFunc2`
-      )
+      );
       expect(execStub).to.have.been.calledWith(
         `go build -ldflags="-s -w" -o .bin/testFunc2 functions/func2/main.go`
-      )
-      expect(execStub.firstCall.args[1].cwd).to.equal('.')
-      expect(execStub.firstCall.args[1].env.GOOS).to.equal('linux')
-      expect(execStub.firstCall.args[1].env.CGO_ENABLED).to.equal('0')
+      );
+      expect(execStub.firstCall.args[1].cwd).to.equal(".");
+      expect(execStub.firstCall.args[1].env.GOOS).to.equal("linux");
+      expect(execStub.firstCall.args[1].env.CGO_ENABLED).to.equal("0");
       expect(config.service.functions.testFunc3.handler).to.equal(
         `.bin/testFunc3`
-      )
+      );
       expect(execStub).to.have.been.calledWith(
         `go build -ldflags="-s -w" -o .bin/testFunc3 functions/func3`
-      )
-    })
+      );
+    });
 
-    it('compiles Go function w/ custom command', async () => {
+    it("compiles Go function w/ custom command", async () => {
       // given
       const config = merge(
         {
           service: {
             custom: {
               go: {
-                cmd: 'CGO_ENABLED=1 GOOS=linux go build'
-              }
+                cmd: "CGO_ENABLED=1 GOOS=linux go build",
+              },
             },
             functions: {
               testFunc1: {
-                name: 'testFunc1',
-                runtime: 'go1.x',
-                handler: 'functions/func1/main.go'
-              }
-            }
-          }
+                name: "testFunc1",
+                runtime: "go1.x",
+                handler: "functions/func1/main.go",
+              },
+            },
+          },
         },
         serverlessStub
-      )
-      const plugin = new Plugin(config)
+      );
+      const plugin = new Plugin(config);
 
       // when
-      await plugin.hooks['before:package:createDeploymentArtifacts']()
+      await plugin.hooks["before:package:createDeploymentArtifacts"]();
 
       // then
       expect(execStub).to.have.been.calledOnceWith(
         `go build -o .bin/testFunc1 functions/func1/main.go`
-      )
-      expect(execStub.firstCall.args[1].env.CGO_ENABLED).to.equal('1')
-      expect(execStub.firstCall.args[1].env.GOOS).to.equal('linux')
-    })
+      );
+      expect(execStub.firstCall.args[1].env.CGO_ENABLED).to.equal("1");
+      expect(execStub.firstCall.args[1].env.GOOS).to.equal("linux");
+    });
 
-    it('compiles Go function w/ custom base dir', async () => {
+    it("compiles Go function w/ custom base dir", async () => {
       // given
       const config = merge(
         {
           service: {
             custom: {
               go: {
-                baseDir: 'gopath'
-              }
+                baseDir: "gopath",
+              },
             },
             functions: {
               testFunc1: {
-                name: 'testFunc1',
-                runtime: 'go1.x',
-                handler: 'functions/func1/main.go'
-              }
-            }
-          }
+                name: "testFunc1",
+                runtime: "go1.x",
+                handler: "functions/func1/main.go",
+              },
+            },
+          },
         },
         serverlessStub
-      )
-      const plugin = new Plugin(config)
+      );
+      const plugin = new Plugin(config);
 
       // when
-      await plugin.hooks['before:package:createDeploymentArtifacts']()
+      await plugin.hooks["before:package:createDeploymentArtifacts"]();
 
       // then
       expect(execStub).to.have.been.calledOnceWith(
         `go build -ldflags="-s -w" -o ../.bin/testFunc1 functions/func1/main.go`
-      )
-      expect(execStub.firstCall.args[1].cwd).to.equal('gopath')
-    })
+      );
+      expect(execStub.firstCall.args[1].cwd).to.equal("gopath");
+    });
 
-    it('compiles Go function w/ global runtime defined', async () => {
+    it("compiles Go function w/ global runtime defined", async () => {
       // given
       const config = merge(
         {
           service: {
             provider: {
-              runtime: 'go1.x'
+              runtime: "go1.x",
             },
             functions: {
               testFunc1: {
-                name: 'testFunc1',
-                handler: 'functions/func1/main.go'
-              }
-            }
-          }
+                name: "testFunc1",
+                handler: "functions/func1/main.go",
+              },
+            },
+          },
         },
         serverlessStub
-      )
-      const plugin = new Plugin(config)
+      );
+      const plugin = new Plugin(config);
 
       // when
-      await plugin.hooks['before:package:createDeploymentArtifacts']()
+      await plugin.hooks["before:package:createDeploymentArtifacts"]();
 
       // then
-      expect(execStub).to.have.been.calledOnce
-    })
+      expect(execStub).to.have.been.calledOnce;
+    });
 
-    it('package Go function individually', async () => {
+    it("package Go function individually", async () => {
       // given
       const config = merge(
         {
           service: {
             functions: {
               testFunc1: {
-                name: 'testFunc1',
-                runtime: 'go1.x',
-                handler: 'functions/func1/main.go'
-              }
-            }
-          }
+                name: "testFunc1",
+                runtime: "go1.x",
+                handler: "functions/func1/main.go",
+              },
+            },
+          },
         },
         serverlessStub
-      )
-      const plugin = new Plugin(config)
+      );
+      const plugin = new Plugin(config);
 
       // when
-      await plugin.hooks['before:package:createDeploymentArtifacts']()
+      await plugin.hooks["before:package:createDeploymentArtifacts"]();
 
       // then
       expect(config.service.functions.testFunc1.package).to.deep.equal({
         individually: true,
         exclude: [`./**`],
-        include: [`.bin/testFunc1`]
-      })
-    })
+        include: [`.bin/testFunc1`],
+      });
+    });
 
-    it('package Go function allowing including more files', async () => {
+    it("package Go function allowing including more files", async () => {
       // given
       const config = merge(
         {
           service: {
             functions: {
               testFunc1: {
-                name: 'testFunc1',
-                runtime: 'go1.x',
-                handler: 'functions/func1/main.go',
+                name: "testFunc1",
+                runtime: "go1.x",
+                handler: "functions/func1/main.go",
                 package: {
-                  include: ['somepath']
-                }
-              }
-            }
-          }
+                  include: ["somepath"],
+                },
+              },
+            },
+          },
         },
         serverlessStub
-      )
-      const plugin = new Plugin(config)
+      );
+      const plugin = new Plugin(config);
 
       // when
-      await plugin.hooks['before:package:createDeploymentArtifacts']()
+      await plugin.hooks["before:package:createDeploymentArtifacts"]();
 
       // then
       expect(config.service.functions.testFunc1.package).to.deep.equal({
         individually: true,
-        exclude: ['./**'],
-        include: [`.bin/testFunc1`, 'somepath']
-      })
-    })
+        exclude: ["./**"],
+        include: [`.bin/testFunc1`, "somepath"],
+      });
+    });
 
-    it('exits if compilation fails', async () => {
+    it("exits if compilation fails", async () => {
       // given
-      execStub.throws()
-      sandbox.stub(process, 'exit')
+      execStub.throws();
+      sandbox.stub(process, "exit");
       const config = merge(
         {
           service: {
             functions: {
               testFunc1: {
-                name: 'testFunc1',
-                runtime: 'go1.x',
-                handler: 'functions/func1/error.go'
-              }
-            }
-          }
+                name: "testFunc1",
+                runtime: "go1.x",
+                handler: "functions/func1/error.go",
+              },
+            },
+          },
         },
         serverlessStub
-      )
-      const plugin = new Plugin(config)
+      );
+      const plugin = new Plugin(config);
 
       // when
-      await plugin.hooks['before:package:createDeploymentArtifacts']()
+      await plugin.hooks["before:package:createDeploymentArtifacts"]();
 
       // then
-      expect(process.exit).to.have.been.called
-    })
+      expect(process.exit).to.have.been.called;
+    });
 
-    it('compiles single Go function', async () => {
+    it("compiles single Go function", async () => {
       // given
       const config = merge(
         {
           service: {
             functions: {
               testFunc1: {
-                name: 'testFunc1',
-                runtime: 'go1.x',
-                handler: 'functions/func1/main.go'
-              }
-            }
-          }
+                name: "testFunc1",
+                runtime: "go1.x",
+                handler: "functions/func1/main.go",
+              },
+            },
+          },
         },
         serverlessStub
-      )
-      const plugin = new Plugin(config, { function: 'testFunc1' })
+      );
+      const plugin = new Plugin(config, { function: "testFunc1" });
 
       // when
-      await plugin.hooks['before:deploy:function:packageFunction']()
+      await plugin.hooks["before:deploy:function:packageFunction"]();
 
       // then
       expect(execStub).to.have.been.calledOnceWith(
         `go build -ldflags="-s -w" -o .bin/testFunc1 functions/func1/main.go`
-      )
-      expect(execStub.firstCall.args[1].cwd).to.equal('.')
-    })
-  })
+      );
+      expect(execStub.firstCall.args[1].cwd).to.equal(".");
+    });
+  });
 
   describe(`serverless go build`, () => {
-    it('compiles Go functions', async () => {
+    it("compiles Go functions", async () => {
       // given
       const config = merge(
         {
           service: {
             functions: {
               testFunc1: {
-                name: 'testFunc1',
-                runtime: 'go1.x',
-                handler: 'functions/func2/main.go'
-              }
-            }
-          }
+                name: "testFunc1",
+                runtime: "go1.x",
+                handler: "functions/func2/main.go",
+              },
+            },
+          },
         },
         serverlessStub
-      )
-      const plugin = new Plugin(config)
+      );
+      const plugin = new Plugin(config);
 
       // when
-      await plugin.hooks['go:build:build']()
+      await plugin.hooks["go:build:build"]();
 
       // then
       expect(config.service.functions.testFunc1.handler).to.equal(
         `.bin/testFunc1`
-      )
-    })
-  })
-})
+      );
+    });
+  });
+});
 
 const serverlessStub = {
   service: {
-    service: 'testService',
-    functions: {}
+    service: "testService",
+    functions: {},
   },
   cli: {
-    consoleLog() {}
-  }
-}
+    consoleLog() {},
+  },
+};
