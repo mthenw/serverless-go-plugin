4aa9ab5bca7e4f46044dcbfe592bcc432c34117f
openhoangnc <hoangnc.jp@gmail.com>
2021-09-15 18:18:56 +0700

    add GOARCH amd64 to build cmd

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const ConfigDefaults = {
   baseDir: ".",
   binDir: ".bin",
   cgo: 0,
-  cmd: 'GOOS=linux go build -ldflags="-s -w"',
+  cmd: 'GOOS=linux GOARCH=amd64 go build -ldflags="-s -w"',
   monorepo: false,
 };
 
